e8a4003fde6619e8c5bbd138198b2a2ec80390dc
WillyValle <you@example.com>
2025-08-27 01:39:08 +0000

    Conexion a base de datos y configuracion completa de docker-compose. Se agregó un test de conexión a la base de datos.

--- backend/app/modelos/database.php
+++ backend/app/modelos/database.php
@@ -1,20 +1,29 @@
 <?php
 
 class BasedeDatos{
-    const servidor = "mysql";
-    const usuariobd = "root";
-    const clave = "rootpass";
-    const nombrebd = "laceibadb";
+    /*const servidor = "mysql";
+    const usuariobd = "USR_APP_LACEIBA_DB";
+    const clave = "L7IOeIRN3LPkt";
+    const nombrebd = "APP_La_Ceiba_DB";*/
 
     public static function Conectar(){
+        $host = getenv('DB_HOST');
+        $dbname = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+
         try{
             $conexion = new PDO
-            ("mysql:host=".self::servidor.";dbname=".self::nombrebd.
-            ";charset=utf8", self::usuariobd, self::clave);
+            ("mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+                $user,
+                $pass
+            );
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conexion;
         }catch(PDOException $e){
-            return "Error de conexion".$e->getMessage();
+            echo "❌ Error de conexión: " . $e->getMessage();
+            return null; // <- importante
+            //return "Error de conexion".$e->getMessage();
         }
     }
 }
